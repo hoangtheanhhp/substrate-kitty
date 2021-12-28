b5b7dbf9a5a526af94d405cf4d6c2815e88fe957
アイン <hoangtheanhhp@gmail.com>
2021-12-28 10:56:44 +0900

    add

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -1,166 +1,74 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://substrate.dev/docs/en/knowledgebase/runtime/frame>
 pub use pallet::*;
-
-mod mock;
-mod tests;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::pallet_prelude::*;
+	use frame_support::{dispatch::*, pallet_prelude::*};
 	use frame_system::pallet_prelude::*;
-	use frame_support::{
-		sp_runtime::traits::Hash,
-		traits::{ Randomness, Currency, tokens::ExistenceRequirement },
-		transactional
-	};
-	use sp_io::hashing::blake2_128;
-	use scale_info::TypeInfo;
-	use std::collections::HashMap;
 
-	#[cfg(feature = "std")]
-	use serde::{Deserialize, Serialize};
-
-	type AccountOf<T> = <T as frame_system::Config>::AccountId;
-	type BalanceOf<T> =
-		<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
-
-	type QuantityType = u16;
-	// Struct for Gift information.
-	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo)]
-	#[scale_info(skip_type_params(T))]
-
-
-	// Set Gift type in Gift struct.
-	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo)]
-	#[scale_info(skip_type_params(T))]
-	#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-	pub enum Gift {
-		Rose,
-		Petty,
-		Cream,
-		Hat,
-		Kiss,
+	pub enum Zodiac {
+		Aquarius,
+		Pisces,
+		Aries,
+		Taurus,
+		Gemini,
+		Cancer,
+		Leo,
+		Virgo,
+		Libra,
+		Scorpio,
+		Sagittarius,
+		Capricorn,
 	}
 
-	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
-	pub struct Pallet<T>(_);
-
-	// Configure the pallet by specifying the parameters and types on which it depends.
+	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 	}
 
-	// Errors.
-	#[pallet::error]
-	pub enum Error<T> {
-		/// Handles arithemtic overflow when incrementing the Kitty counter.
-		GiftCntZero,
-		/// Handles checking that the Kitty is owned by the account transferring, buying or setting a price for it.
-		NotGiftOwner,
-	}
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
 
+	// The pallet's runtime storage items.
+	// https://docs.substrate.io/v3/runtime/storage
+	#[pallet::storage]
+	#[pallet::getter(fn account_zodiac)]
+	//Account's zodiac storage
+	pub type AccountZodiac<T> = StorageMap<_, Blake2_128Concat, T::AccountId, Zodiac, ValueQuery>;
+
+
+	// Pallets use events to inform users when important changes are made.
+	// https://docs.substrate.io/v3/runtime/events-and-errors
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		Donated(T::AccountId, T::AccountId, Gift, QuantityType),
-	}
-	#[pallet::storage]
-	#[pallet::getter(fn gifts_owned)]
-	/// Keeps track of what accounts own what Kitty.
-	pub(super) type GiftsOwned<T: Config> =
-		StorageMap<_, Twox64Concat, T::AccountId, BoundedVec<Gift, QuantityType>>;
-	// Our pallet's genesis configuration.
-	#[pallet::genesis_config]
-	pub struct GenesisConfig<T: Config> {
-		pub kitties: Vec<(T::AccountId, [u8; 16], Gender)>,
+		SetZodiacSucceed(T::AccountId, Zodiac),
 	}
 
-	// Required to implement default for GenesisConfig.
-	#[cfg(feature = "std")]
-	impl<T: Config> Default for GenesisConfig<T> {
-		fn default() -> GenesisConfig<T> {
-			GenesisConfig { kitties: vec![] }
-		}
+	// Errors inform users that something went wrong.
+	#[pallet::error]
+	pub enum Error<T> {
+		ZodiacWasSet,
 	}
-
-	#[pallet::genesis_build]
-	impl<T: Config> GenesisBuild<T> for GenesisConfig<T> {
-		fn build(&self) {
-			// When building a kitty from genesis config, we require the dna and gender to be supplied.
-			for (acct, dna, gender) in &self.kitties {
-				let _ = <Pallet<T>>::mint(acct, Some(dna.clone()), Some(gender.clone()));
-			}
-		}
-	}
-
-	#[pallet::hooks]
-	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
-
-	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		#[pallet::weight(100)]
-		pub fn donate(
-			origin: OriginFor<T>,
-			to: T::AccountId,
-			gift: Gift,
-			quantity: QuantityType,
-		) -> DispatchResult {
-			let from = ensure_signed(origin)?;
-
-			// Verify the kitty is not transferring back to its owner.
-			ensure!(from != to, <Error<T>>::DonateToSelf);
-
-			Self::execute_donate(from, to, gift, quantity)?;
-
-			Self::deposit_event(Event::Donated(from, to, gift, quantity));
-
-			Ok(())
-		}
-	}
-
-	//** Our helper functions.**//
-
-	impl<T: Config> Pallet<T> {
-		#[transactional]
-		pub fn execute_donate(
-			from: &T::AccountId,
-			to: &T::AccountId,
-			gift: Gift,
-			quantity: QuantityType,
-		) -> Result<(), Error<T>> {
-
-			// Remove `kitty_id` from the KittyOwned vector of `prev_kitty_owner`
-			<KittiesOwned<T>>::try_mutate(&prev_owner, |owned| {
-				if let Some(ind) = owned.iter().position(|&id| id == *kitty_id) {
-					owned.swap_remove(ind);
-					return Ok(());
-				}
-				Err(())
-			}).map_err(|_| <Error<T>>::KittyNotExist)?;
-
-			// Update the kitty owner
-			kitty.owner = to.clone();
-			// Reset the ask price so the kitty is not for sale until `set_price()` is called
-			// by the current owner.
-			kitty.price = None;
-
-			<Kitties<T>>::insert(kitty_id, kitty);
-
-			<KittiesOwned<T>>::try_mutate(to, |vec| {
-				vec.try_push(*kitty_id)
-			}).map_err(|_| <Error<T>>::ExceedMaxKittyOwned)?;
-
+		/// An example dispatchable that takes a singles value as a parameter, writes the value to
+		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+		#[pallet::weight(10_000)]
+		pub fn setZodiac(origin: OriginFor<T>, zodiac: Zodiac) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+			if AccountZodiac::<T>::contrain_key(&account_id) {
+				Err(Error::<T>::ZodiacWasSet)?
+			} else {
+				AccountZodiac::<T>::insert(&who, zodiac);
+				Self::deposit_event(Event::SetZodiacSucceed(&who, zodiac));
+			}
+			// Return a successful DispatchResultWithPostInfo
 			Ok(())
 		}
 	}
